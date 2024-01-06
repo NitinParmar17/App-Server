774693513fae64aa51214848fea82166d82e5c4f
NitinParmar17 <np74323@gmail.com>
2024-01-06 20:44:36 +0530

    redis connection fixed

--- routes/user-api.js
+++ routes/user-api.js
@@ -5,7 +5,10 @@ const connection = require('../dbConnection.js');
 const redis = require("redis");
 const redis_port = process.env.PORT || 6379;
 
-const redisClient = redis.createClient(redis_port);
+const redisClient = redis.createClient({
+    legacyMode: true,
+    PORT: redis_port
+});
 
 // const client =
 // http://localhost:5000/createUser/John/1234567890
@@ -15,19 +18,13 @@ const redisClient = redis.createClient(redis_port);
 // router.get('/createUser', (req, res) => {
 
 
-
-redisClient.on('connect', () => {
-    console.log('Redis client connected');
-    // You can start executing commands on redisClient here
+redisClient.connect().then(() => {
+    console.log('Connected to Redis successfully!');
+}).catch((err) => {
+    console.error('Error connecting to Redis:', err);
 });
 
-redisClient.on('error', (err) => {
-    console.error(`Redis Error: ${err}`);
-});
-
-
-
-
+// using redis
 router.get('/createUser/:name/:phone', (req, res) => {
     try {
         console.log("abc");
@@ -112,53 +109,6 @@ router.get('/getUser/:phone', (req, res) => {
         res.status(500).json({ error: 'Server error' });
     }
 });
-
-
-
-// router.get('/getUser/:phone', (req, res) => {
-//     try {
-//         let number = req.params.phone;
-
-//         redisClient.get(number, (err, reply) => {
-//             if (err) {
-//                 // Handle error if the get operation fails
-//                 console.error('Error getting value from Redis:', err);
-//             } else {
-//                 // If the key exists, reply will contain the value associated with 'myKey'
-//                 console.log('Value from Redis:', reply);
-//             }
-//         });
-
-//         const query = `SELECT name FROM user where phone = '${number}'`;
-
-//         connection.query(query, (error, results) => {
-//             console.log("hello");
-//             if (error) {
-//                 console.error('Error fetching data:', error);
-//                 res.status(500).json({ error: 'Error fetching data' });
-//                 return;
-//             } else {
-//                 let name = results[0].name;
-
-//                 redisClient.set(number, name, (err, reply) => {
-//                     if (err) {
-//                         // Handle error if the set operation fails
-//                         console.error('Error setting value in Redis:', err);
-//                     } else {
-//                         // Confirmation that the value was set successfully
-//                         console.log('Value set in Redis:', reply);
-//                     }
-//                 });
-
-//                 res.json({ message: 'User Found', user: { name, number } });
-
-//             }
-//         });
-//     }
-//     catch (ex) {
-//         console.log(ex);
-//     }
-// });
 
 module.exports = router;
 
